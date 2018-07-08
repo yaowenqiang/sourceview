fb97adb033d8094282df9df76913dab3ace2cff9
yaowenqiang <yaowenqiang111@163.com>
2018-07-08 16:45:09 +0000

    添加了文件行数，md5,ctime,atime,等，修复一个typo

--- index.php
+++ index.php
@@ -8,15 +8,25 @@
     //$finder->depth('== 0');
     $client = ClientBuilder::create()->build();
 
-
+    $finfo = new finfo(FILEINFO_MIME,'/usr/share/misc/magic');
     foreach ($finder as $file) {
         $doc = [];
         $doc['realpath'] = $file->getRealPath(); 
         $doc['relativepath'] = $file->getRelativePath(); 
         $doc['relativepathname'] = $file->getRelativePathname();
-        $doc['countent'] = $file->getContents(); 
+        $doc['ctime'] = $file->getCTime(); 
+        $doc['atime'] = $file->getATime(); 
+        $doc['md5'] = md5($doc['relativepathname']); 
+        $doc['filename'] = $file->getFilename(); 
+        $doc['content'] = $file->getContents(); 
         $doc['extension'] = $file->getExtension(); 
         $doc['size'] = $file->getSize(); 
+        $fileinfo = $finfo->file($doc['relativepathname']);
+        $doc['istext'] = substr($fileinfo, 0, 4) == 'text';
+        if($doc['istext']) {
+            $doc['lines'] = getLineNumber($doc['relativepathname']);
+        }
+        
         $params = [
             'index'=>'my_index',
             'type'=>'my_type',
@@ -25,6 +35,20 @@
         ];
     $response = $client->index($params);
     var_dump($response);
-
+        
     }
 
+    function getLineNumber($file='') {
+        if ($file) {
+            $linecount = 0;
+            $handle = fopen($file, "r");
+            while(!feof($handle)){
+              $line = fgets($handle);
+              $linecount++;
+            }
+            fclose($handle);
+            return $linecount;
+        } else {
+            return 0;
+        }
+    }    
